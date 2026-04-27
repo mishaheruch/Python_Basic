b13dce77108a70afbd078c4f9c0cc53f65dac832
Misga Herych <misha_gerich@icloud.com>
2026-04-27 18:32:35 +0300

    add tasks for lesson 2

---
+++ lesson2/final_digit_task.py
@@ -0,0 +1,3 @@
+number = int(input("Введіть число:"))
+
+print("Остання цифра:", number % 10)
---
+++ lesson2/vyvedennya_chysla_v_stovpchyk.py
@@ -0,0 +1,11 @@
+n = int(input("Введіть число:"))
+
+d1 = n // 1000
+d2 = n // 100 % 10
+d3 = n // 10 % 10
+d4 = n % 10
+
+print(d1)
+print(d2)
+print(d3)
+print(d4)
