7b899d3338b419a67df31ebc4250de1c612d06d2
Misga Herych <misha_gerich@icloud.com>
2026-05-03 21:55:23 +0300

    refactor split_list_into_two logic for clarity and improve outputs

--- lesson3/split_list_into_two.py
+++ lesson3/split_list_into_two.py
@@ -1,25 +1,13 @@
-# [1, 2, 3, 4, 5, 6] => [[1, 2, 3], [4, 5, 6]]
-# [1, 2, 3] => [[1, 2], [3]]
-# [1, 2, 3, 4, 5] => [[1, 2, 3], [4, 5]]
-# [1] => [[1], []]
-# [] => [[], []]
-#
 # [1, 2, 3, 4, 5, 6]
 # [1, 2, 3]
 # [1, 2, 3, 4, 5]
 # [1]
 # []
 
-num_list = []
+num_list = [1, 2, 3]
 z = len(num_list)
 
-if z % 2 == 0:
-    num_list = [num_list[: z // 2]] + [num_list[z // 2 :]]
-    print(num_list)
-elif z % 2 != 0:
-    num_list = [num_list[: z // 2]] + [num_list[z // 2 :]]
-    print(num_list)
-elif z == 1:
+if z == 1:
     num_list = [
         [][num_list[0]],
     ]
@@ -27,6 +15,12 @@ elif z == 1:
 elif z == 0:
     num_list = [[][num_list[0]]]
     print(num_list)
+
+else:
+    first_part = num_list[: (z + 1) // 2]
+    second_part = num_list[(z + 1) // 2 :]
+    result = [first_part, second_part]
+    print(result)
 
 # num_list = [1]
 #
