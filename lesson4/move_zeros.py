db254e6268fa3a4526a2d1e576f20ae15774d729
Misga Herych <misha_gerich@icloud.com>
2026-05-06 21:20:15 +0300

    fix: update variable names in move_zeros task for clarity

--- lesson4/move_zeros.py
+++ lesson4/move_zeros.py
@@ -4,13 +4,13 @@
 # [1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
 # [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 
-list = [0, 1, 0, 12, 3]
+number = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 
-zero_count = list.count(0)
+zero_count = number.count(0)
 
 for i in range(zero_count):
-    zero_index = list.index(0)
-    list.pop(zero_index)
-    list.append(zero_count * 0)
+    zero_index = number.index(0)
+    number.pop(zero_index)
+    number.append(zero_count * 0)
 
-print(list)
+print(number)
