acd15b3d8aa0d31ad053d3ee06f4c78534a70d0b
Misga Herych <misha_gerich@icloud.com>
2026-05-10 21:26:50 +0300

    Added hashtag

---
+++ lesson5/hashtag.py
@@ -0,0 +1,11 @@
+import string
+
+my_string = input("Введіт речення: ")
+
+my_string = my_string.title()
+my_string = my_string.replace(" ", "")
+
+for x in string.punctuation:
+    my_string = my_string.replace(x, "")
+
+print(my_string[:140])
