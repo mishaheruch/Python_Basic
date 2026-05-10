6804e36726aab4c626804901f5bc5e21a5b1e7a5
Misga Herych <misha_gerich@icloud.com>
2026-05-10 18:13:29 +0300

    Added for

--- lesson3/simple_calculator.py
+++ lesson3/simple_calculator.py
@@ -1,21 +1,29 @@
-first_number = float(input("Введіт перше число : "))
-second_number = float(input("Введіт друге число : "))
+yes = "y"
 
-operation = input("Вибиріть знак(+, -, *, /): ")
+while yes == "y" or yes == "Y" or yes == "yes" or yes == "Yes" or yes == "YES":
 
-if operation == "+":
-    result = first_number + second_number
-elif operation == "-":
-    result = first_number - second_number
-elif operation == "*":
-    result = first_number * second_number
-elif operation == "/":
-    if second_number == 0:
-        print("Діленя на 0 не можливе")
-        exit()
-    result = first_number / second_number
-else:
-    result = 0
-    print("Невідомий оператор")
+    yes = ""
 
-print("Результат:", result)
+    first_number = float(input("Введіт перше число : "))
+    second_number = float(input("Введіт друге число : "))
+
+    operation = input("Вибиріть знак(+, -, *, /): ")
+
+    if operation == "+":
+        result = first_number + second_number
+    elif operation == "-":
+        result = first_number - second_number
+    elif operation == "*":
+        result = first_number * second_number
+    elif operation == "/":
+        if second_number == 0:
+            print("Діленя на 0 не можливе")
+            exit()
+        result = first_number / second_number
+    else:
+        result = 0
+        print("Невідомий оператор")
+
+    print("Результат:", result)
+
+    yes = input("Якщо ви хочите продовжити напишіть y, якщо ні то no: ")
