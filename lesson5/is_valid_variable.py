484a7370c31e8b9ace8291246299ba28e25cdceb
Misga Herych <misha_gerich@icloud.com>
2026-05-17 20:47:39 +0300

    fix variable name validation logic

--- lesson5/is_valid_variable.py
+++ lesson5/is_valid_variable.py
@@ -1,39 +1,32 @@
-# _ => True
-# __ => False
-# ___ => False
-# x => True
-# get_value => True
-# get value => False
-# get!value => False
-# some_super_puper_value => True
-# Get_value => False
-# get_Value => False
-# getValue => False
-# 3m => False
-# m3 => True
-# assert => False
-# assert_exception => True
-
 import string
 import keyword
 
-z = str(input())
+z = str(input("Введіть назву зміної:"))
 
-if z[0].isdigit():
-    print("false")
-    exit()
-elif not z.count("_") != 1:
-    if not z.islower():
-        print("false")
-        exit()
-elif "__" in z:
-    print("false")
-    exit()
-
-for char in string.punctuation:
+for char in string.punctuation + " ":
     if char == "_":
         continue
     elif char in z:
-        print("false")
+        print("False")
         exit()
-print(z not in keyword.kwlist)
+for x in z:
+    if not x.isalpha():
+        continue
+    if x.isupper():
+        print("False")
+        exit()
+if z == "_":
+    print("True")
+    exit()
+elif z[0].isdigit():
+    print("False")
+    exit()
+elif "__" in z:
+    print("False")
+    exit()
+else:
+    if z not in keyword.kwlist:
+        print("True")
+    else:
+        print("False")
+        exit()
