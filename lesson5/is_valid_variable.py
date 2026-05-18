a374ceb502acc4e71f1be0299cb69e2017626c25
Misga Herych <misha_gerich@icloud.com>
2026-05-18 19:10:31 +0300

    fix

--- lesson5/is_valid_variable.py
+++ lesson5/is_valid_variable.py
@@ -1,32 +1,34 @@
 import string
 import keyword
 
-z = str(input("Введіть назву зміної:"))
+z = str(input("Введіть назву змінної: "))
 
 for char in string.punctuation + " ":
     if char == "_":
         continue
     elif char in z:
-        print("False")
+        print(False)
         exit()
+
 for x in z:
     if not x.isalpha():
         continue
     if x.isupper():
-        print("False")
+        print(False)
         exit()
+
 if z == "_":
-    print("True")
+    print(True)
     exit()
 elif z[0].isdigit():
-    print("False")
+    print(False)
     exit()
 elif "__" in z:
-    print("False")
+    print(False)
     exit()
 else:
     if z not in keyword.kwlist:
-        print("True")
+        print(True)
     else:
-        print("False")
+        print(False)
         exit()
